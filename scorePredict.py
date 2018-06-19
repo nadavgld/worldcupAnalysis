d52e6c188b4ffbc3bc9630a612083652b1e5a796
Nadav grinberg <nadavgld@gmail.com>
2018-06-19 17:58:58 +0300

    nn-model via keras + visual arrangement

--- scorePredict.py
+++ scorePredict.py
@@ -8,47 +8,48 @@ from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
 from keras.utils import np_utils
 
-# raw_data = pd.read_csv("WorldCup 2018.csv")
 raw_data = util.get_csv()
 
-#target data
+# target data
 home_target = raw_data["Home_Score"]
 away_target = raw_data["Away_Score"]
 
-#data to analyse
+# data to analyse
 home_data = ['WDW_ Home', 'PRDZ_Home', 'FORE_Home', 'VITI_Home', 'FREESUPERTIPS_Home', 'SCOREPREDICTOR_Home']
 away_data = ["WDW_Away", "PRDZ_Away", "FORE_Away", "VITI_Away", "FREESUPERTIPS_Away", "SCOREPREDICTOR_Away"]
 home_var = raw_data[home_data]
 away_var = raw_data[away_data]
 
-#only played games to build the model
+# only played games to build the model
 gamesPlayedIndex = home_target.count()
 
-home_target = home_target.iloc[:gamesPlayedIndex,]
-away_target = away_target.iloc[:gamesPlayedIndex,]
-home_pred = home_var.iloc[gamesPlayedIndex:gamesPlayedIndex+1,]
-away_pred = away_var.iloc[gamesPlayedIndex:gamesPlayedIndex+1,]
-home_var = home_var.iloc[:gamesPlayedIndex,]
-away_var = away_var.iloc[:gamesPlayedIndex,]
+home_target = home_target.iloc[:gamesPlayedIndex, ]
+away_target = away_target.iloc[:gamesPlayedIndex, ]
+home_pred = home_var.iloc[gamesPlayedIndex:gamesPlayedIndex + 1, ]
+away_pred = away_var.iloc[gamesPlayedIndex:gamesPlayedIndex + 1, ]
+home_var = home_var.iloc[:gamesPlayedIndex, ]
+away_var = away_var.iloc[:gamesPlayedIndex, ]
 
+currentGame = raw_data[gamesPlayedIndex:gamesPlayedIndex + 1]
+print "\n----------------\n\n    " + currentGame['Home'].values[0] + " vs " + currentGame['Away'].values[0]
 
-#models
+# models
 
-#random forest
+# random forest
 rf_home_scoreModel = RandomForestRegressor(n_estimators=100, criterion='mae').fit(home_var, home_target)
 rf_away_scoreModel = RandomForestRegressor(n_estimators=100, criterion='mae').fit(away_var, away_target)
 rf_home_pred = rf_home_scoreModel.predict(home_pred)
 rf_away_pred = rf_away_scoreModel.predict(away_pred)
-print("random forest: ", rf_home_pred[0], rf_away_pred[0])
+print "\nRandom-Forest: \n\t", int(round(rf_home_pred[0])), "-", int(round(rf_away_pred[0]))
 
-#LinearRegression
+# LinearRegression
 lr_home_scoreModel = LinearRegression().fit(home_var, home_target)
 lr_away_scoreModel = LinearRegression().fit(away_var, away_target)
 lr_home_pred = lr_home_scoreModel.predict(home_pred)
 lr_away_pred = lr_away_scoreModel.predict(away_pred)
-print("linear regression: ", lr_home_pred[0], lr_away_pred[0])
+print "\nLinear-Regression:\n\t", int(round(lr_home_pred[0])), "-", int(round(lr_away_pred[0]))
 
-#nn
+# nn
 # def baseline_model():
 #     nn = Sequential()
 #     nn.add(Dense(10, input_dim=6, activation='relu'))
@@ -56,12 +57,12 @@ print("linear regression: ", lr_home_pred[0], lr_away_pred[0])
 #     nn.compile(loss='mean_squared_error', optimizer='adam')
 #     return nn
 
-#nn_est = KerasRegressor(build_fn=baseline_model, epochs = 100, batch_size=3)
-#nn_est.fit(home_var, home_target)
-#nn_est.predict(home_pred)
-#print(nn_est.predict(home_pred))
+# nn_est = KerasRegressor(build_fn=baseline_model, epochs = 100, batch_size=3)
+# nn_est.fit(home_var, home_target)
+# nn_est.predict(home_pred)
+# print(nn_est.predict(home_pred))
 
-print("nn model")
+print("\nnn-model:")
 model = Sequential()
 model.add(Dense(4, input_dim=6, activation='relu'))
 model.add(Dense(4, activation='relu'))
@@ -71,5 +72,9 @@ model.fit(home_var, home_target, epochs=300, verbose=0)
 nn_home = model.predict(home_pred)
 model.fit(away_var, away_target, epochs=300, verbose=0)
 nn_away = model.predict(away_pred)
-print(nn_home[0])
-print(nn_away[0])
+
+f_home = int(round(float(str(nn_home[0][0]))))
+f_away = int(round(float(str(nn_away[0][0]))))
+print "\t" + str(f_home) + " - " + str(f_away)
+
+print "\n----------------"
