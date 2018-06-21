7bd99810567fa799de4e8dc5f7a5ebe20f54bdbc
Nadav grinberg <nadavgld@gmail.com>
2018-06-21 16:26:15 +0300

    Modules separate

--- scorePredict.py
+++ scorePredict.py
@@ -1,80 +1,96 @@
-import pandas as pd
-import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 import util
+import winnerPredict
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasRegressor
-from keras.utils import np_utils
+import os
 
-raw_data = util.get_csv()
+os.system("cls")
+def run():
+    raw_data = util.get_csv()
 
-# target data
-home_target = raw_data["Home_Score"]
-away_target = raw_data["Away_Score"]
+    # target data
+    home_target = raw_data["Home_Score"]
+    away_target = raw_data["Away_Score"]
 
-# data to analyse
-home_data = ['WDW_ Home', 'PRDZ_Home', 'FORE_Home', 'VITI_Home', 'FREESUPERTIPS_Home', 'SCOREPREDICTOR_Home']
-away_data = ["WDW_Away", "PRDZ_Away", "FORE_Away", "VITI_Away", "FREESUPERTIPS_Away", "SCOREPREDICTOR_Away"]
-home_var = raw_data[home_data]
-away_var = raw_data[away_data]
+    # data to analyse
+    home_data = ['WDW_ Home', 'PRDZ_Home', 'FORE_Home', 'VITI_Home', 'FREESUPERTIPS_Home', 'SCOREPREDICTOR_Home']
+    away_data = ["WDW_Away", "PRDZ_Away", "FORE_Away", "VITI_Away", "FREESUPERTIPS_Away", "SCOREPREDICTOR_Away"]
+    home_var = raw_data[home_data]
+    away_var = raw_data[away_data]
 
-# only played games to build the model
-gamesPlayedIndex = home_target.count()
+    # only played games to build the model
+    gamesPlayedIndex = home_target.count()
 
-home_target = home_target.iloc[:gamesPlayedIndex, ]
-away_target = away_target.iloc[:gamesPlayedIndex, ]
-home_pred = home_var.iloc[gamesPlayedIndex:gamesPlayedIndex + 1, ]
-away_pred = away_var.iloc[gamesPlayedIndex:gamesPlayedIndex + 1, ]
-home_var = home_var.iloc[:gamesPlayedIndex, ]
-away_var = away_var.iloc[:gamesPlayedIndex, ]
+    home_target = home_target.iloc[:gamesPlayedIndex, ]
+    away_target = away_target.iloc[:gamesPlayedIndex, ]
+    home_pred = home_var.iloc[gamesPlayedIndex:gamesPlayedIndex + 1, ]
+    away_pred = away_var.iloc[gamesPlayedIndex:gamesPlayedIndex + 1, ]
+    home_var = home_var.iloc[:gamesPlayedIndex, ]
+    away_var = away_var.iloc[:gamesPlayedIndex, ]
 
-currentGame = raw_data[gamesPlayedIndex:gamesPlayedIndex + 1]
-print "\n----------------\n\n    " + currentGame['Home'].values[0] + " vs " + currentGame['Away'].values[0]
+    currentGame = raw_data[gamesPlayedIndex:gamesPlayedIndex + 1]
+    print "\n----------------\n\n    " + currentGame['Home'].values[0] + " vs " + currentGame['Away'].values[0]
 
-# models
+    # models
 
-# random forest
-rf_home_scoreModel = RandomForestRegressor(n_estimators=100, criterion='mae').fit(home_var, home_target)
-rf_away_scoreModel = RandomForestRegressor(n_estimators=100, criterion='mae').fit(away_var, away_target)
-rf_home_pred = rf_home_scoreModel.predict(home_pred)
-rf_away_pred = rf_away_scoreModel.predict(away_pred)
-print "\nRandom-Forest: \n\t", int(round(rf_home_pred[0])), "-", int(round(rf_away_pred[0])), "\n w/o round: ", round(rf_home_pred[0],2), "-", round(rf_away_pred[0],2)
+    # random forest
+    rf_home_scoreModel = RandomForestRegressor(n_estimators=100, criterion='mae').fit(home_var, home_target)
+    rf_away_scoreModel = RandomForestRegressor(n_estimators=100, criterion='mae').fit(away_var, away_target)
+    rf_home_pred = rf_home_scoreModel.predict(home_pred)
+    rf_away_pred = rf_away_scoreModel.predict(away_pred)
+    print "\nRandom-Forest: \n\t", int(round(rf_home_pred[0])), "-", int(
+        round(rf_away_pred[0])), "\n w/o round: ", round(
+        rf_home_pred[0], 2), "-", round(rf_away_pred[0], 2)
 
-# LinearRegression
-lr_home_scoreModel = LinearRegression().fit(home_var, home_target)
-lr_away_scoreModel = LinearRegression().fit(away_var, away_target)
-lr_home_pred = lr_home_scoreModel.predict(home_pred)
-lr_away_pred = lr_away_scoreModel.predict(away_pred)
-print "\nLinear-Regression:\n\t", int(round(lr_home_pred[0])), "-", int(round(lr_away_pred[0])), "\n w/o round: ", round(lr_home_pred[0],2), "-", round(lr_away_pred[0],2)
+    # LinearRegression
+    lr_home_scoreModel = LinearRegression().fit(home_var, home_target)
+    lr_away_scoreModel = LinearRegression().fit(away_var, away_target)
+    lr_home_pred = lr_home_scoreModel.predict(home_pred)
+    lr_away_pred = lr_away_scoreModel.predict(away_pred)
+    print "\nLinear-Regression:\n\t", int(round(lr_home_pred[0])), "-", int(
+        round(lr_away_pred[0])), "\n w/o round: ", round(lr_home_pred[0], 2), "-", round(lr_away_pred[0], 2)
 
-# nn
-# def baseline_model():
-#     nn = Sequential()
-#     nn.add(Dense(10, input_dim=6, activation='relu'))
-#     nn.add(Dense(1))
-#     nn.compile(loss='mean_squared_error', optimizer='adam')
-#     return nn
+    # nn
+    # def baseline_model():
+    #     nn = Sequential()
+    #     nn.add(Dense(10, input_dim=6, activation='relu'))
+    #     nn.add(Dense(1))
+    #     nn.compile(loss='mean_squared_error', optimizer='adam')
+    #     return nn
 
-# nn_est = KerasRegressor(build_fn=baseline_model, epochs = 100, batch_size=3)
-# nn_est.fit(home_var, home_target)
-# nn_est.predict(home_pred)
-# print(nn_est.predict(home_pred))
+    # nn_est = KerasRegressor(build_fn=baseline_model, epochs = 100, batch_size=3)
+    # nn_est.fit(home_var, home_target)
+    # nn_est.predict(home_pred)
+    # print(nn_est.predict(home_pred))
 
-print("\nnn-model:")
-model = Sequential()
-model.add(Dense(4, input_dim=6, activation='relu'))
-model.add(Dense(1, activation='relu'))
-model.add(Dense(1, activation='linear'))
-model.compile(loss='mse', optimizer='adam')
-model.fit(home_var, home_target, epochs=300, verbose=0)
-nn_home = model.predict(home_pred)
-model.fit(away_var, away_target, epochs=300, verbose=0)
-nn_away = model.predict(away_pred)
+    print("\nnn-model:")
+    model = Sequential()
+    model.add(Dense(4, input_dim=6, activation='relu'))
+    model.add(Dense(1, activation='relu'))
+    model.add(Dense(1, activation='linear'))
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(home_var, home_target, epochs=300, verbose=0)
+    nn_home = model.predict(home_pred)
+    model.fit(away_var, away_target, epochs=300, verbose=0)
+    nn_away = model.predict(away_pred)
 
-f_home = int(round(float(str(nn_home[0][0]))))
-f_away = int(round(float(str(nn_away[0][0]))))
-print "\t" + str(f_home) + " - " + str(f_away), "\n w/o round: ", str(round(nn_home[0][0],2)), "-", str(round(nn_away[0][0],2))
+    f_home = int(round(float(str(nn_home[0][0]))))
+    f_away = int(round(float(str(nn_away[0][0]))))
+    print "\t" + str(f_home) + " - " + str(f_away), "\n w/o round: ", str(round(nn_home[0][0], 2)), "-", str(
+        round(nn_away[0][0], 2))
 
-print "\n----------------"
+    winnerPredict.prepare_data(raw_data)
+
+    print "\n----------------"
+
+
+run()
+
+rerun = raw_input("Re-run?(y/n)")
+while rerun.lower() == "y":
+    run()
+    rerun = raw_input("Re-run?(y/n)")
+
+print "byebye:)"
--- winnerPredict.py
+++ winnerPredict.py
@@ -3,14 +3,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
 
-raw_data = util.get_csv()
 
-
-home_target = raw_data["Home_Score"]
-away_target = raw_data["Away_Score"]
-gamesPlayedIndex = home_target.count()
-
-def teamSign(home,away):
+def teamSign(home, away):
     if (home > away):
         return 1
     elif (away > home):
@@ -18,47 +12,73 @@ def teamSign(home,away):
     else:
         return 2
 
+
 def explain(score):
-    if (score==1):
+    if (score == 1):
         return "home wins"
-    elif (score==2):
+    elif (score == 2):
         return "draw"
     elif (score == 3):
         return "away wins"
     return "nadav"
 
 
-raw_data['WDW_tri'] = raw_data.apply(lambda row: teamSign(row['WDW_ Home'], row['WDW_Away']), axis=1)
-raw_data['PDRZ_tri'] = raw_data.apply(lambda row: teamSign(row['PRDZ_Home'], row['PRDZ_Away']), axis=1)
-raw_data['FORE_tri'] = raw_data.apply(lambda row: teamSign(row['FORE_Home'], row['FORE_Away']), axis=1)
-raw_data['VITI_tri'] = raw_data.apply(lambda row: teamSign(row['VITI_Home'], row['VITI_Away']), axis=1)
-raw_data['FREESUPERTIPS_tri'] = raw_data.apply(lambda row: teamSign(row['FREESUPERTIPS_Home'], row['FREESUPERTIPS_Away']), axis=1)
-raw_data['SCOREPREDICTOR_tri'] = raw_data.apply(lambda row: teamSign(row['SCOREPREDICTOR_Home'], row['SCOREPREDICTOR_Away']), axis=1)
-raw_data['target_tri'] = raw_data.apply(lambda row: teamSign(row['Home_Score'], row['Away_Score']), axis=1)
+def prepare_data(raw_data):
+    # raw_data = util.get_csv()
+    home_target = raw_data["Home_Score"]
+    away_target = raw_data["Away_Score"]
+    gamesPlayedIndex = home_target.count()
 
-model_var = raw_data[['WDW_tri','PDRZ_tri','FORE_tri','VITI_tri', 'FREESUPERTIPS_tri','SCOREPREDICTOR_tri']]
-#print(raw_data[['WDW_ Home', 'WDW_Away', 'WDW_tri']])
+    raw_data['WDW_tri'] = raw_data.apply(lambda row: teamSign(row['WDW_ Home'], row['WDW_Away']), axis=1)
+    raw_data['PDRZ_tri'] = raw_data.apply(lambda row: teamSign(row['PRDZ_Home'], row['PRDZ_Away']), axis=1)
+    raw_data['FORE_tri'] = raw_data.apply(lambda row: teamSign(row['FORE_Home'], row['FORE_Away']), axis=1)
+    raw_data['VITI_tri'] = raw_data.apply(lambda row: teamSign(row['VITI_Home'], row['VITI_Away']), axis=1)
+    raw_data['FREESUPERTIPS_tri'] = raw_data.apply(
+        lambda row: teamSign(row['FREESUPERTIPS_Home'], row['FREESUPERTIPS_Away']), axis=1)
+    raw_data['SCOREPREDICTOR_tri'] = raw_data.apply(
+        lambda row: teamSign(row['SCOREPREDICTOR_Home'], row['SCOREPREDICTOR_Away']), axis=1)
+    raw_data['target_tri'] = raw_data.apply(lambda row: teamSign(row['Home_Score'], row['Away_Score']), axis=1)
 
-train_var = model_var.iloc[:gamesPlayedIndex, ]
-train_target = raw_data['target_tri'].iloc[:gamesPlayedIndex, ]
-pred_var = model_var.iloc[gamesPlayedIndex:gamesPlayedIndex + 1, ]
+    model_var = raw_data[['WDW_tri', 'PDRZ_tri', 'FORE_tri', 'VITI_tri', 'FREESUPERTIPS_tri', 'SCOREPREDICTOR_tri']]
 
-#random forest model
-rf_class = RandomForestClassifier(n_estimators=300, criterion='entropy', max_depth=2, min_samples_split=5).fit(train_var, train_target)
-rf_result = rf_class.predict(pred_var)
-rf_prob = rf_class.predict_proba(pred_var)
-print "rf class: ", explain(rf_result)
-print " home prob.: ", round(rf_prob[0][0],2), " away prob.: ", round(rf_prob[0][2],2), " draw prob.: ", round(rf_prob[0][1],2)
+    train_var = model_var.iloc[:gamesPlayedIndex, ]
+    train_target = raw_data['target_tri'].iloc[:gamesPlayedIndex, ]
+    pred_var = model_var.iloc[gamesPlayedIndex:gamesPlayedIndex + 1, ]
 
-#KNN
-knn_class = KNeighborsClassifier(n_neighbors=3, weights='distance').fit(train_var, train_target)
-knn_results = knn_class.predict(pred_var)
-knn_prob = knn_class.predict_proba(pred_var)
-print "knn class: ", explain(knn_results)
-print " home prob.: ", round(knn_prob[0][0],2), " away prob.: ", round(knn_prob[0][2],2), " draw prob.: ", round(knn_prob[0][1],2)
+    knn_classify(train_var, train_target, pred_var)
+    random_forest_classify(train_var, train_target, pred_var)
 
-svc_class = LinearSVC().fit(train_var, train_target)
-svc_results = svc_class.predict(pred_var)
-#svc_prob = svc_class.predict_proba(pred_var)
-print "svc class: ", explain(svc_results)
-#print " home prob.: ", round(svc_prob[0][0],2), " away prob.: ", round(svc_prob[0][2],2), " draw prob.: ", round(svc_prob[0][1],2)
+
+# random forest model
+def random_forest_classify(train_var, train_target, pred_var):
+    rf_class = RandomForestClassifier(n_estimators=300, criterion='entropy', max_depth=2, min_samples_split=5).fit(
+        train_var, train_target)
+    rf_result = rf_class.predict(pred_var)
+    rf_prob = rf_class.predict_proba(pred_var)
+    print "\nRandom-Forest classify: ", explain(rf_result)
+    print "Home prob.: ", round(rf_prob[0][0], 2), " Away prob.: ", round(rf_prob[0][2], 2), " Draw prob.: ", round(
+        rf_prob[0][1], 2)
+
+    print "\n----------------\n"
+
+    sum_res = 0
+    for feat in range(0, len(rf_class.feature_importances_)):
+        r = rf_class.feature_importances_[feat] * pred_var.iloc[:, feat]
+        sum_res = sum_res + r
+
+    print "Random-Forest feature importance: " + explain(int(round(sum_res.iloc[0])))
+
+
+# KNN
+def knn_classify(train_var, train_target, pred_var):
+    knn_class = KNeighborsClassifier(n_neighbors=3, weights='distance').fit(train_var, train_target)
+    knn_results = knn_class.predict(pred_var)
+    knn_prob = knn_class.predict_proba(pred_var)
+    print "\nKNN classify: ", explain(knn_results)
+    print "Home prob.: ", round(knn_prob[0][0], 2), " Away prob.: ", round(knn_prob[0][2], 2), " Draw prob.: ", round(
+        knn_prob[0][1], 2)
+
+    svc_class = LinearSVC().fit(train_var, train_target)
+    svc_results = svc_class.predict(pred_var)
+    # svc_prob = svc_class.predict_proba(pred_var)
+    print "\nSVC classify: ", explain(svc_results)
